619aacac33e0e51ee2629908cbc73aa365c94e6d
buigabor <bui.gabor97@gmail.com>
2020-09-11 16:16:12 +0200

    Refactor getRandomNumberBetween

--- app.ts
+++ app.ts
@@ -82,10 +82,10 @@ prevBtn.addEventListener("click", () => {
 })
 
 randomBtn.addEventListener("click", () => {
-    let randomNum = getRandomNumberBetween(0, 3)
+    let randomNum = getRandomNumberBetween();
     showPerson(randomNum);
 })
 
-function getRandomNumberBetween(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function getRandomNumberBetween(): number {
+    return Math.floor(Math.random() * reviews.length);
 }
